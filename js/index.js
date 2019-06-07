15800e0fee9f8fd59c192adf8d2079f770ca4d1d
Kayla Dailey <kayladailey000@gmail.com>
2019-06-07 12:51:36 -0700

    added image and changed navigation color

--- js/index.js
+++ js/index.js
@@ -47,12 +47,12 @@ links.forEach ((links) => {
 });
 
 const nav = document.querySelectorAll('a');
-nav [0].createElement  = 'Services';
-nav [1].createElement  = 'Product';
-nav [2].createElement  = 'Vision';
-nav [3].createElement  = 'Features';
-nav [4].createElement  = 'About';
-nav [5].createElement  = 'Contact';
+nav [0].textContent  = 'Services';
+nav [1].textContent  = 'Product';
+nav [2].textContent  = 'Vision';
+nav [3].textContent  = 'Features';
+nav [4].textContent  = 'About';
+nav [5].textContent  = 'Contact';
 
 
 
@@ -63,28 +63,41 @@ const buttonText = document.querySelector('button');
 buttonText.textContent = "Get Started";
 
 const cta = document.querySelector('h1');
-cta.textContent = "Dom is Awesome!";
+cta.innerHTML = "Dom <br> is <br> Awesome!";
+
+const section = document.querySelector('section');
+const domImg = document.createElement('img');
+domImg.src = "pictures//ddomimg.jpg"
+section.append(domImg)
+
+
+const titleColor = document.querySelectorAll('h4')
+titleColor.forEach ((titleColor) => {
+ titleColor.style.fontSize
+});
 
 const title = document.querySelectorAll('h4');
 
-title [0].createElement = 'Features';
-title [1].createElement = 'About';
-title [2].createElement = 'Services';
-title [3].createElement = 'Product';
-title [4].createElement= 'Vision';
-title [5].createElement = 'Contact';
+title [0].textContent = 'Features';
+title [1].textContent = 'About';
+title [2].textContent = 'Services';
+title [3].textContent = 'Product';
+title [4].textContent = 'Vision';
+title [5].textContent = 'Contact';
 
 const par = document.querySelectorAll('p');
 
 const midImg = document.getElementById('middle-img');
 midImg.src = "img/mid-page-accent.jpg"
 
-par [0].createElement = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, interdutm nisi.Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-par [1].createElement  = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-par [2].createElement  = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-par [3].createElement  = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-par [4].createElement  = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
-par [5].createElement  = "123 Way 456 Street";
-par [6].createElement  = "Somewhere, USA";
-par [7].createElement  = "1 (888) 888-8888";
+
+
+par [0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, interdutm nisi.Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [1].textContent  = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [2].textContent  = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [3].textContent  = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [4].textContent  = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [5].textContent  = "123 Way 456 Street";
+par [6].textContent  = "Somewhere, USA";
+par [7].textContent  = "1 (888) 888-8888";
 
