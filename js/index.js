bc2b4ee9c7f6d38b062cef688c5032a185618163
Kayla Dailey <kayladailey000@gmail.com>
2019-06-06 21:21:45 -0700

    Completed transferring my DOM to the JS file

--- js/index.js
+++ js/index.js
@@ -40,3 +40,51 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const links = document.querySelectorAll('a');
+links.forEach ((links) => {
+  links.style.color = "green"
+});
+
+const nav = document.querySelectorAll('a');
+nav [0].createElement  = 'Services';
+nav [1].createElement  = 'Product';
+nav [2].createElement  = 'Vision';
+nav [3].createElement  = 'Features';
+nav [4].createElement  = 'About';
+nav [5].createElement  = 'Contact';
+
+
+
+const codeImage = document.getElementById('cta-img');
+codeImage.src = "img/header-img.png"
+
+const buttonText = document.querySelector('button');
+buttonText.textContent = "Get Started";
+
+const cta = document.querySelector('h1');
+cta.textContent = "Dom is Awesome!";
+
+const title = document.querySelectorAll('h4');
+
+title [0].createElement = 'Features';
+title [1].createElement = 'About';
+title [2].createElement = 'Services';
+title [3].createElement = 'Product';
+title [4].createElement= 'Vision';
+title [5].createElement = 'Contact';
+
+const par = document.querySelectorAll('p');
+
+const midImg = document.getElementById('middle-img');
+midImg.src = "img/mid-page-accent.jpg"
+
+par [0].createElement = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, interdutm nisi.Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [1].createElement  = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [2].createElement  = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [3].createElement  = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [4].createElement  = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+par [5].createElement  = "123 Way 456 Street";
+par [6].createElement  = "Somewhere, USA";
+par [7].createElement  = "1 (888) 888-8888";
+
